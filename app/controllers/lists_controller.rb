3ff59c40efa949736d8e8a9ef3d2e68214741555
David Deville <david.deville59@gmail.com>
2022-11-17 16:09:09 +0100

    added API features

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,44 @@
+class ListsController < ApplicationController
+
+  before_action :set_list, only: ['index']
+  before_action :set_bookmark, only: ['show']
+
+  # Display all list
+  def index
+    @lists = List.all
+  end
+
+  # Display a single list
+  def show
+    @list = List.find(params[:id])
+  end
+
+  # Create a new list
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+
+  def set_list
+    @list = List.new
+  end
+
+  def set_bookmark
+    @bookmark = Bookmark.new
+  end
+end
